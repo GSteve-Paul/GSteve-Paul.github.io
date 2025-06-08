dbd615c13a8c0945e94ad03a55a6890dd6963768
GSteve-Paul <lijn101@nenu.edu.cn>
2025-06-08 20:14:35 +0800

    Quartz sync: Jun 8, 2025, 8:14 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,8 +24,7 @@ export const sharedPageComponents: SharedLayout = {
     ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/GSteve-Paul",
     },
   }),
 }
